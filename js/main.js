72bf07c24389157c8816fe88994cb8653c74e132
ravindragsingh <arien.641@gmail.com>
2018-11-29 11:31:18 -0500

    mainJs

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
     const image = document.createElement('img');
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
+    image.setAttribute('alt', `An image of ${restaurant.name}`);
     li.append(image);
 
     const name = document.createElement('h1');
@@ -178,6 +179,7 @@ createRestaurantHTML = (restaurant) => {
     const more = document.createElement('a');
     more.innerHTML = 'View Details';
     more.href = DBHelper.urlForRestaurant(restaurant);
+    more.setAttribute('aria-label', `More details about ${restaurant.name}`)
     li.append(more)
 
     return li
